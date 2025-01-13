3d654dbaa716c54518d1f5ecd6b9ec5bb8bce58b
daotq1 <v.daotq1@vinhomes.vn>
2025-01-13 10:34:00 +0700

    update

--- js/common.js
+++ js/common.js
@@ -1,10 +1,1 @@
-$(document).ready(function () {
-    $.get("../header.html", function (data, status) {
-        console.log(data);
-        $('#masthead').html(data);
-    });
-    $.get("../footer.html", function (data, status) {
-        console.log(data);
-        $('#contact-us').html(data);
-    });
-});
+$(document).ready((function(){$.get("../header.html",(function(data,status){console.log(data),$("#masthead").html(data)})),$.get("../footer.html",(function(data,status){console.log(data),$("#contact-us").html(data)}))}));
